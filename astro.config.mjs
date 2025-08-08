a374a9e153ad89a982554c7f7554af2b8d67e51f
micolg <micolg@gmail.com>
2025-08-09 01:04:17 +0200

    update path

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,6 @@
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-  site: 'https://micula1312.github.io/aotu-site/', // se usi custom domain, metti quello
+  site: 'https://micula1312.github.io/aotu-site/',
+  base: '/aotu-site/',
 });
